df98afefbe4a9981a0efd16e7de1ecb92d521c00
PFallesen <pf@rff.dk>
2019-12-11 16:08:05 +0100

    Back on track w. proper sample - I owe Lars Pico a beer

--- tstf_to_r_resubmit.R
+++ tstf_to_r_resubmit.R
@@ -1,13 +1,14 @@
 
 
-install.packages(c("usethis"),dep=TRUE)
-install.packages('tseries',dep=TRUE)
-install.packages('rio',dep=TRUE)
-install.packages('taRifx',dep=TRUE)
-install.packages('qpcR',dep=TRUE)
-install.packages(c("TSA","car","aod","forecast","statsDK","tidyverse","lmtest"),dep=TRUE)
-install.packages("Rtools",dep=TRUE)
-rm(list=ls())
+#install.packages(c("usethis"),dep=TRUE)
+#install.packages('tseries',dep=TRUE)
+#install.packages('rio',dep=TRUE)
+#install.packages('taRifx',dep=TRUE)
+#install.packages('qpcR',dep=TRUE)
+#install.packages(c("TSA","car","aod","forecast","statsDK","tidyverse","lmtest"),dep=TRUE)
+#install.packages("Rtools",dep=TRUE)
+#install.packages("statsDK",dep=TRUE)
+#rm(list=ls())
 library(plyr)
 library(usethis)
 library(stats)
@@ -20,7 +21,7 @@ library(tseries)
 library(haven)
 library(rio)
 library(zoo)
-library(statsDK)
+#library(statsDK)
 library(tidyverse)
 library(taRifx)
 library(qpcR)
@@ -164,6 +165,9 @@ mydata$pulse <- as.numeric(mydata$year == 2013 & mydata$month==10)
 
 mydata$step <- as.numeric((mydata$year >= 2013 & mydata$month>=10) |(mydata$year >= 2014) )
 
+mydata$p98 <- as.numeric(mydata$x == 98)
+mydata$p133 <- as.numeric(mydata$x==133)
+
 
 #Combine TS-data with denominator dataset
 mydata <- merge(mydata,marriages,by="year",all=TRUE)
@@ -174,7 +178,7 @@ rm(list=c("marriages","info","bevc3","data","marr","variable_overview","variable
           "folk1a_info","unmarried2007"))
 
 #saves a copy of the estimation sample
-write.csv2(mydata,file="sample.csv")
+#write.csv2(mydata,file="sample.csv")
 mydata<-read.csv2(file="sample.csv")
 
 #divorce rate measured as monthly divorce per 100,000 married couples mid year
@@ -318,11 +322,12 @@ dev.off()
 
 
 
-#Run ITSD model with step-increase in divorce risk
+#Run ITSD model with differencing and MA(1)
 #Model 2 in paper
 t6 <- arimax(logy, order = c(0,1,1), seasonal = c(1,0,0),
              io=c(79,97,132),
-             xtransf = mydata[,c("pulse","step")], transfer = list(c(1,0),c(0,0)))
+             xtransf = mydata[,c("pulse","step")
+                              ], transfer = list(c(1,0),c(0,0)))
 aicc6<-AICc(t6)
 aicc6
 summary(t6)
@@ -331,10 +336,17 @@ tsdiag(t6, gof=24, tol = 0.1, col = "red", omit.initial = TRUE)
 shapiro.test(t6$residuals)
 runs(t6$residuals)
 checkresiduals(t6,lag=24)
+adf.test(t6$residuals)
 
+mydata$p98 <-data.frame(p<-as.numeric(mydata$X==98))
+mydata$p133 <-data.frame(p<-as.numeric(mydata$X==133))
+
+#Run ITSD model with pulse effects after AOs
+#Model 2 in paper
 t7 <- arimax(logy, order = c(0,0,0), seasonal = c(1,0,0),
              io=c(79,97,132),
-             xtransf = mydata[,c("pulse","step")], transfer = list(c(1,0),c(0,0)))
+             xtransf = mydata[,c("pulse","step","p98","p133")
+                              ], transfer = list(c(1,0),c(0,0),c(1,0),c(1,0)))
 aicc7<-AICc(t7)
 aicc7
 summary(t7)
@@ -343,3 +355,4 @@ tsdiag(t7, gof=24, tol = 0.1, col = "red", omit.initial = TRUE)
 shapiro.test(t7$residuals)
 runs(t7$residuals)
 checkresiduals(t7,lag=24)
+adf.test(t7$residuals)
